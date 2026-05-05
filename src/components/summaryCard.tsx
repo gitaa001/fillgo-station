88558fb5961953ca9d3ab340527eb542462ebe9b
gitaa001 <18223122@std.stei.itb.ac.id>
2026-05-06 00:14:54 +0700

    fix icontype

--- src/components/summaryCard.tsx
+++ src/components/summaryCard.tsx
@@ -1,23 +1,21 @@
-import { ReactNode, isValidElement, cloneElement } from "react";
-
+import { IconType } from "react-icons";
 
 type Props = {
   title: string;
   value: number;
   color: string;
-  icon?: ReactNode;
+  icon?: IconType;
 };
 
-export default function SummaryCard({ title, value, color, icon }: Props) {
+export default function SummaryCard({ title, value, color, icon: Icon }: Props) {
   return (
     <div className={`flex-1 rounded-xl p-4 text-white ${color} flex flex-col`}>
-        {icon && (
-          <div className="mb-3 flex">
-            {isValidElement(icon)
-              ? cloneElement(icon, { size: 24, color: "white" })
-              : icon}
-          </div>
-        )}
+      {Icon && (
+        <div className="mb-3 flex">
+          <Icon size={24} color="white" />
+        </div>
+      )}
+
       <h2 className="text-2xl font-bold">{value}</h2>
       <p className="text-sm">{title}</p>
     </div>
